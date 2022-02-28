d7c6fbfefe85a2596e1841c49e2af3f0b9c8090d
C APPS GITHUB TEAM <84335028+capps096github@users.noreply.github.com>
2022-03-01 00:14:03 +0300

    Minor adjustments and adding the theme colors to tailwindjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "utb-blue": "#0772ba",
+        "utb-green": "#00a651",
+      },
+    },
     fontFamily: {
       spartan: ["Spartan", "sans-serif"],
       lato: ["Lato", "sans-serif"],

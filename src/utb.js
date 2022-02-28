f8dbddc808a03a21da28c65bc658e166588d7c21
C APPS GITHUB TEAM <84335028+capps096github@users.noreply.github.com>
2022-02-28 18:34:11 +0300

    Added the Score Settings too

---
+++ src/utb.js
@@ -0,0 +1,9 @@
+// * Go to Score Settings
+function goToScoreSettings() {
+  location.href = "score_settings.html";
+}
+
+// * Go to Score Settings
+function goToHome() {
+  location.href = "home.html";
+}

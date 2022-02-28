b83c6b032375922ca7f823cd150875226f7ca317
C APPS GITHUB TEAM <84335028+capps096github@users.noreply.github.com>
2022-02-28 17:00:05 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      spartan: ["Spartan", "sans-serif"],
+      lato: ["Lato", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
